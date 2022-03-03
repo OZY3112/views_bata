f0b9d4d61b8c84308e42674b47de82db45e21ee2
OZY <92400495+OZY3112@users.noreply.github.com>
2022-03-03 08:49:15 +0200

    init

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import {IoMdSearch} from 'react-icons/io'
 
 export default function MoviesPage() {
   return (
     <section id="moviespage">
+      <div className="movie__search--section--cont">
       <div className="movies__section--title--cont">
         <h5 className="movies__section--title">
             Browse all of your favorite movies and shows 
         </h5>  
       </div> 
-    <input type="text" name="" id="" className="movies__searchbar" placeholder='Search for a movie' />
+        <div className="movies__search-bar--cont">
+          <input type="text" name="" className="movies__search-bar" placeholder='Search for a movie' />
+          <div className="movies__search-bar--icon--cont">
+            <IoMdSearch/>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
